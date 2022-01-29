3ba934b5108b58a2a7d030f8075562c49edcda63
Keying Chen <35384984+KKyInGG@users.noreply.github.com>
2022-01-29 13:11:00 -0500

    MouseSpawn

---
+++ Assets/Script/MouseFollower.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollower : MonoBehaviour
+{
+    private Transform _transform;
+    private Vector3 _cursorPos;
+    private Camera _cam;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        _transform = GetComponent<Transform>();
+        _cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Vector3 position = _cam.ScreenToWorldPoint(Input.mousePosition);
+        _transform.position = _cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5));
+        
+    }
+
+    
+
+
+}
